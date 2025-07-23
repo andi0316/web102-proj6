5a04d6db6090c2831588be168000cf02f529e458
andi0316 <andriasubhit@gmail.com>
2025-07-23 02:39:48 -0400

    updated legend

--- src/components/StatsChart.jsx
+++ src/components/StatsChart.jsx
@@ -15,10 +15,9 @@ export default function StatBarChart({ pokemonList }) {
   return (
     <ResponsiveContainer width="100%" height={300}>
       <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 40 }}>
-        <XAxis />
+        <XAxis dataKey="name" angle={-45} textAnchor="end" interval={0} />
         <YAxis />
-        <Tooltip />
-        <Legend />
+        <Tooltip dataKey="name" angle={-45} textAnchor="end" interval={0}/>
         <Bar dataKey="HP" fill="#8884d8" />
         <Bar dataKey="Attack" fill="#82ca9d" />
         <Bar dataKey="Defense" fill="#ffc658" />
